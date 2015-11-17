641007a11474f6ca97c1d5900a6eefe99d83e452
hezhao <hezhao@users.noreply.github.com>
2015-11-17 12:36:30 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ setup(
         name='grooveshark',
         version='0.1',
         description='Simple python library of Grooveshark Public API v3',
-        author='He Zhao',
-        author_email='james_littlefox@hotmail.com',
         url='https://github.com/hezhao/Grooveshark',
         packages=find_packages()
     )
